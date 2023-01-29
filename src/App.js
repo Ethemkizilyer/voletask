67627d73c32b98968afbff8cb8395eef6bacdc3e
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2023-01-29 07:59:27 +0300

    feat: added carousel components and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import CarouselSlider from "./Components/CarouselSlider";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-     
+      <Header />
+      <CarouselSlider />
     </div>
   );
 }
